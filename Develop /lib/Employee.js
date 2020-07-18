87c018a20b1cc2ed04dab845bbf7b5579c24df5b
Nico Michel <nicomichel@utexas.edu>
2020-07-18 15:33:46 -0500

    created prompts for employee question file, cannot get it to run at the moment

--- Develop /lib/Employee.js
+++ Develop /lib/Employee.js
@@ -4,7 +4,55 @@ class Employee {
         this.name = name
         this.id = id
         this.email = email
+        this.role = role
     }
-    getName()
-    
-}
+    getName() {
+        return inquirer.prompt ([
+            {
+                type: 'input',
+                name: 'EmpName',
+                message: 'Employee Name?',
+            }
+        ]).then(answers =>{
+            this.name = answers.name 
+            this.getId
+        })
+    }
+    getId(){
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'EmpId',
+                message: 'Employee ID #?',
+            }
+        ]).then(answers=>{
+            this.id = answers.id
+            this.getEmail
+        })
+    }
+    getEmail(){
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'EmpEmail',
+                message: 'Employee Email?',
+            }
+        ]).then(answers=>{
+            this.email = answers.email
+            this.getRole
+        })
+    }
+    getRole(){
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'EmpRole',
+                message: 'Employee Role?',
+            }
+        ]).then(answers=>{
+            this.role = answers.role
+        })
+    }
+}; 
+
+module.exports = Employee
