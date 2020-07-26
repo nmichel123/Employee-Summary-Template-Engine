9f31d8f7c5b22dcd278d2b3165792a86a4179f13
Nico Michel <nicomichel@utexas.edu>
2020-07-25 23:39:24 -0500

    got function to return questions after initial role query, bug it only returns intern q's

--- Develop /app.js
+++ Develop /app.js
@@ -10,6 +10,16 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const chooseChoices = ["Intern", "Engineer", "Manager"]
+
+const choose = [{
+    type: "list",
+    name: "empChoice",
+    // choices: [chooseChoices[0], chooseChoices[1], chooseChoices[2]],
+    choices: ["Intern", "Engineer", "Manager"],
+    message: "What type of employee?"
+}]
+
 const empQuest = [{
     type: "input",
     name: "role",
@@ -95,26 +105,25 @@ const manQuest = [
 },
 ]
 
+async function runPrompt () {
+    inquirer.prompt(choose)
+    .then(answer=> {
+        if (chooseChoices[0]) {
+           return inquirer.prompt(intQuest) 
+        }
+        else if (chooseChoices[1]) {
+           return inquirer.prompt(engQuest)
+        }
+        else if (chooseChoices[2]) {
+           return inquirer.prompt(manQuest)
+        }
+        
+    }
+    )}
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+runPrompt ();
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const finalAns = [];
